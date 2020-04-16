ec68bcf62af55c30cc72bd0a772f87242350dea5
KoudaiMatsumoto <alisov.000@icloud.com>
2020-04-16 15:02:33 +0900

    2020y4m16d

--- easylogin/src/main/java/com/example/easylogin/model/dao/UserRepository.java
+++ easylogin/src/main/java/com/example/easylogin/model/dao/UserRepository.java
@@ -1,5 +1,7 @@
 package com.example.easylogin.model.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.example.easylogin.model.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+	
+	List<User> findByUserNameAndPassword(String userName, String password);
 
 }
